f6dd5f918276533c1f6e390f94a4da78e120571d
Krishna Prasad Regmi <iamkpregmi@gmail.com>
2024-08-31 18:09:37 +0530

    Delete Form_Automation directory

--- Form_Automation/core/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Student(models.Model):
-    student_id = models.CharField(primary_key=True, max_length=5)
-    student_name = models.CharField(max_length=50)
-    city = models.CharField(max_length=15)
-    fee = models.DecimalField(max_digits=9, decimal_places=2)
-
-    
